10951219277e77c191ffd95b607feafaee9e48dd
Wuqi Li <liwuqi2006@gmail.com>
2023-04-28 00:51:48 -0400

    minor fix

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import {createApp} from 'vue'
 import {createRouter, createWebHistory} from 'vue-router'
 
+// 引入pinia，用于状态管理
+import {createPinia} from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 // 引入路由router
 import routes from '@/router/routes'
 import {useAuth} from './stores/auth';
@@ -19,37 +26,35 @@ import './assets/styles/app.scss'
 // 引入App.vue，根组件
 import App from '@/App.vue'
 
-const initApp = (pinia) => {
-    const router = createRouter({
-        history: createWebHistory(import.meta.env.BASE_URL), routes: routes
-    })
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL), routes: routes
+})
 
-    const app = createApp(App)
+router.beforeEach(async (to, from, next) => {
+    const authStore = useAuth()
+    const publicPages = ['/login', '/register']
+    const authRequired = !publicPages.includes(to.path)
+    const loggedIn = await authStore.current();
 
-    app.use(pinia)
+    if (authRequired && !loggedIn) {
+        next('/login')
+    } else if (!authRequired && loggedIn) {
+        next('/')
+    } else {
+        next()
+    }
+})
 
-    app.use(router)
+const app = createApp(App)
 
-    app.use(ElementPlus, {
-        locale: zhCn,
-    })
+app.use(pinia)
 
-    app.mount('#app')
+app.use(router)
 
-    router.beforeEach(async (to, from, next) => {
-        const authStore = useAuth()
-        const publicPages = ['/login', '/register']
-        const authRequired = !publicPages.includes(to.path)
-        const loggedIn = await authStore.current();
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 
-        if (authRequired && !loggedIn) {
-            next('/login')
-        } else if (!authRequired && loggedIn) {
-            next('/')
-        } else {
-            next()
-        }
-    })
-}
+app.mount('#app')
 
-export default initApp
+
