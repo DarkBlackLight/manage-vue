9df58d1f301266a096bf61dfa86edb17e8ae411a
Wuqi Li <liwuqi2006@gmail.com>
2023-04-28 10:05:26 -0400

    minor fix

--- stores/auth.js
+++ stores/auth.js
@@ -4,6 +4,7 @@ import API from '@/api'
 export const useAuth = defineStore('auth', {
     state: () => ({
         user: null,
+        role: null,
         storeConfig: {
             MenuCollapse: false,
             dark: false,
@@ -17,18 +18,26 @@ export const useAuth = defineStore('auth', {
             try {
                 let response = await API.Auth.current();
                 this.user = response.data;
+                this.role = response.data.source.role
                 return true;
             } catch (e) {
                 this.user = null;
+                this.role = null
                 return false;
             }
         },
         async login(user) {
             await API.Auth.validate_email_password(user)
-                .then((response) => this.user = response.data)
+                .then((response) => {
+                    this.user = response.data;
+                    this.role = response.data.source.role;
+                })
         },
         async logout() {
-            await API.Auth.logout().finally(() => this.user = null);
+            await API.Auth.logout().finally(() => {
+                this.user = null
+                this.role = null;
+            });
         },
         changeStoreConfig(key, value) {
             this.storeConfig[key] = value
