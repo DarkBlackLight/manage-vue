f337c58b8b96c7924044da12a09cf3136aaf5c9a
Wuqi Li <liwuqi2006@gmail.com>
2023-04-28 00:12:16 -0400

    minor fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 // 引入路由router
 import routes from '@/router/routes'
-import {useAuth} from './stores/auth';
+// import {useAuth} from './stores/auth';
 
 // 引入element-plus
 import ElementPlus from 'element-plus'
@@ -23,42 +23,39 @@ import '@/assets/css/app.scss'
 // 引入App.vue，根组件
 import App from '@/App.vue'
 
-const initApp = () => {
-    const pinia = createPinia()
-    pinia.use(piniaPluginPersistedstate)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-    const router = createRouter({
-        history: createWebHistory(import.meta.env.BASE_URL), routes: routes
-    })
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL), routes: routes
+})
 
-    const app = createApp(App)
+const app = createApp(App)
 
-    app.use(pinia)
+app.use(pinia)
 
-    app.use(router)
+app.use(router)
 
-    app.use(ElementPlus, {
-        locale: zhCn,
-    })
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 
-    app.mount('#app')
+app.mount('#app')
 
-    router.beforeEach(async (to, from, next) => {
-        const authStore = useAuth()
-        const publicPages = ['/login', '/register']
-        const authRequired = !publicPages.includes(to.path)
-        const loggedIn = await authStore.current();
 
-        if (authRequired && !loggedIn) {
-            next('/login')
-        } else if (!authRequired && loggedIn) {
-            next('/')
-        } else {
-            next()
-        }
-    })
-    
-    return app
-}
+// router.beforeEach(async (to, from, next) => {
+//     const authStore = useAuth()
+//     const publicPages = ['/login', '/register']
+//     const authRequired = !publicPages.includes(to.path)
+//     const loggedIn = await authStore.current();
+//
+//     if (authRequired && !loggedIn) {
+//         next('/login')
+//     } else if (!authRequired && loggedIn) {
+//         next('/')
+//     } else {
+//         next()
+//     }
+// })
 
-export default initApp
+
