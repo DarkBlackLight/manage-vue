fad877de3e459fc364c6a3edfd494be3b908cc08
Wuqi Li <liwuqi2006@gmail.com>
2023-04-27 20:35:28 -0400

    minor fix

---
+++ components/Resources/index.js
@@ -0,0 +1,9 @@
+import ResourceDialog from "./ResourceDialog";
+import ResourceForm from "./ResourceForm";
+import ResourceIndex from "./ResourceIndex";
+
+export default {
+    ResourceDialog,
+    ResourceForm,
+    ResourceIndex
+}
