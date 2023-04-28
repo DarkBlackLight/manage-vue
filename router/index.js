3310ed14d7d5b251cda115b5729870f540486358
Wuqi Li <liwuqi2006@gmail.com>
2023-04-27 23:21:45 -0400

    minor fix

--- router/index.js
+++ router/index.js
@@ -3,23 +3,28 @@ import {useAuth} from '../stores/auth'
 
 import routes from '@/router/routes'
 
-const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL), routes: routes
-})
 
-router.beforeEach(async (to, from, next) => {
-    const authStore = useAuth()
-    const publicPages = ['/login', '/register']
-    const authRequired = !publicPages.includes(to.path)
-    const loggedIn = await authStore.current();
+const initRouter = () => {
+    const router = createRouter({
+        history: createWebHistory(import.meta.env.BASE_URL),
+        routes: routes
+    })
 
-    if (authRequired && !loggedIn) {
-        next('/login')
-    } else if (!authRequired && loggedIn) {
-        next('/')
-    } else {
-        next()
-    }
-})
+    router.beforeEach(async (to, from, next) => {
+        const authStore = useAuth()
+        const publicPages = ['/login', '/register']
+        const authRequired = !publicPages.includes(to.path)
+        const loggedIn = await authStore.current();
 
-export default router
+        if (authRequired && !loggedIn) {
+            next('/login')
+        } else if (!authRequired && loggedIn) {
+            next('/')
+        } else {
+            next()
+        }
+    })
+
+    return router
+}
+export default initRouter
