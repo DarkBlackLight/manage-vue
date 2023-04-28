2ed936505d25bb6e022e7aeb348665dfaec40f77
Wuqi Li <liwuqi2006@gmail.com>
2023-04-27 23:44:49 -0400

    merge files

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 import {createApp} from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import {createPinia} from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import routes from '@/router/routes'
+import {useAuth} from './stores/auth';
 
 // 引入pinia，用于状态管理
-import pinia from './stores'
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 // 引入路由router
-import router from './router'
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL), routes: routes
+})
+router.beforeEach(async (to, from, next) => {
+    const authStore = useAuth()
+    const publicPages = ['/login', '/register']
+    const authRequired = !publicPages.includes(to.path)
+    const loggedIn = await authStore.current();
+
+    if (authRequired && !loggedIn) {
+        next('/login')
+    } else if (!authRequired && loggedIn) {
+        next('/')
+    } else {
+        next()
+    }
+})
 
 // 引入App.vue，根组件
 import App from '@/App.vue'
@@ -19,6 +41,7 @@ import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 //全局样式文件
 import '@/assets/css/main.scss'
 import '@/assets/css/app.scss'
+import {useAuth} from "./stores/auth";
 
 const app = createApp(App)
 
