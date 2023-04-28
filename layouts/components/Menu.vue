c106ae63dca9343539d038f3f0a83d6db0f1d56d
Wuqi Li <liwuqi2006@gmail.com>
2023-04-28 00:58:39 -0400

    minor fix

--- layouts/components/Menu.vue
+++ layouts/components/Menu.vue
@@ -34,23 +34,20 @@
 </template>
 
 <script setup>
-import {ref, onMounted, markRaw} from 'vue'
+import {ref, onMounted} from 'vue'
 import {useRouter, useRoute} from 'vue-router'
-import {
-  Grid, Handbag, HomeFilled, UserFilled, Shop,
-  Setting, ShoppingCart, Box, Ticket
-} from '@element-plus/icons-vue'
 
 import {useAuth} from '../../stores/auth';
+import menus from '@/router/menus';
 
-const router = useRouter();
 const route = useRoute();
 
 const defaultActive = ref('')
 
 const authStore = useAuth();
+
 // 导航列表
-const list = ref([])
+const list = ref(menus)
 
 const handleOpen = (key, keyPath) => {
   // console.log(key, keyPath)
@@ -62,95 +59,6 @@ const handleClose = (key, keyPath) => {
 
 onMounted(() => {
   defaultActive.value = route.path.split('/')[1] ? '/' + route.path.split('/')[1] : '/'
-  list.value = [
-    {
-      name: '信息总览',
-      icon: markRaw(HomeFilled),
-      path: '/',
-      children: []
-    },
-    {
-      name: '商品管理',
-      icon: markRaw(Handbag),
-      path: '/products',
-      children: []
-    },
-    {
-      name: '分类管理',
-      icon: markRaw(Grid),
-      path: '/categories',
-      children: []
-    },
-    {
-      name: '商户管理',
-      icon: markRaw(Shop),
-      path: '/shops',
-      children: []
-    },
-    {
-      name: '库存管理',
-      icon: markRaw(Box),
-      children: [
-        {
-          name: '仓库管理',
-          path: '/warehouses',
-        },
-        {
-          name: '库存商品',
-          path: '/warehouse_items',
-        },
-        {
-          name: '入库转移',
-          path: '/movements',
-        },
-      ]
-    },
-    {
-      name: '订单管理',
-      icon: markRaw(ShoppingCart),
-      path: '/orders',
-      children: []
-    },
-    {
-      name: '优惠劵管理',
-      icon: markRaw(Ticket),
-      path: '/coupons',
-      children: []
-    },
-
-    {
-      name: '人员管理',
-      icon: markRaw(UserFilled),
-      children: [
-        {
-          name: '用户管理',
-          path: '/admins',
-        },
-        {
-          name: '客户管理',
-          path: '/customers',
-        }
-      ]
-    },
-    {
-      name: '系统设置',
-      icon: markRaw(Setting),
-      children: [
-        {
-          name: '付款方式',
-          path: '/payment_methods',
-        },
-        {
-          name: '配送方式',
-          path: '/delivery_methods',
-        },
-        {
-          name: '税率管理',
-          path: '/taxes',
-        },
-      ]
-    },
-  ]
 })
 
 </script>
