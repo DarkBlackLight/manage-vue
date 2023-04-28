0cfec97df8f2f63f7205ddabc8b893eae75c25cf
Wuqi Li <liwuqi2006@gmail.com>
2023-04-27 20:52:23 -0400

    add stores

---
+++ stores/auth.js
@@ -0,0 +1,40 @@
+import {defineStore} from 'pinia'
+import API from 'manage-vue/api'
+
+export const useAuth = defineStore('auth', {
+    state: () => ({
+        user: null,
+        storeConfig: {
+            MenuCollapse: false,
+            dark: false,
+        }
+    }),
+    actions: {
+        async current() {
+            if (!this.user)
+                return false;
+
+            try {
+                let response = await API.Auth.current();
+                this.user = response.data;
+                return true;
+            } catch (e) {
+                this.user = null;
+                return false;
+            }
+        },
+        async login(user) {
+            await API.Auth.validate_email_password(user)
+                .then((response) => this.user = response.data)
+        },
+        async logout() {
+            await API.Auth.logout().finally(() => this.user = null);
+        },
+        changeStoreConfig(key, value) {
+            this.storeConfig[key] = value
+        }
+    },
+    persist: {
+        paths: ['user', 'storeConfig']
+    }
+})
