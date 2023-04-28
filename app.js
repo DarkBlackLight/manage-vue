e84eb7bd21805aea637a60d9d687458aa0dd9ae8
Wuqi Li <liwuqi2006@gmail.com>
2023-04-27 23:51:16 -0400

    minor fix

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ import {createRouter, createWebHistory} from 'vue-router'
 import {createPinia} from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
+const app = createApp(App)
+
 // 引入pinia，用于状态管理
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
 
 // 引入路由router
 import routes from '@/router/routes'
@@ -31,25 +35,23 @@ router.beforeEach(async (to, from, next) => {
     }
 })
 
-// 引入App.vue，根组件
-import App from '@/App.vue'
+app.use(router)
 
 // 引入element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
 // 引入element-plus全局国际化的配置
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 
 //全局样式文件
 import '@/assets/css/main.scss'
 import '@/assets/css/app.scss'
 
-const app = createApp(App)
+// 引入App.vue，根组件
+import App from '@/App.vue'
 
-app.use(router)
-app.use(ElementPlus, {
-    locale: zhCn,
-})
-app.use(pinia)
 app.mount('#app')
