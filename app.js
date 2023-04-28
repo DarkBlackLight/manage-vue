e7facde3ab910a060c260ea00b371b3a5ce7610c
Wuqi Li <liwuqi2006@gmail.com>
2023-04-28 00:00:33 -0400

    minor fix

--- app.js
+++ app.js
@@ -1,57 +1,66 @@
 import {createApp} from 'vue'
 import {createRouter, createWebHistory} from 'vue-router'
-import {createPinia} from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
-const app = createApp(App)
 
 // 引入pinia，用于状态管理
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
-
-app.use(pinia)
+import {createPinia} from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 // 引入路由router
 import routes from '@/router/routes'
 import {useAuth} from './stores/auth';
 
-const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL), routes: routes
-})
-
-
-router.beforeEach(async (to, from, next) => {
-    const authStore = useAuth()
-    const publicPages = ['/login', '/register']
-    const authRequired = !publicPages.includes(to.path)
-    const loggedIn = await authStore.current();
-
-    if (authRequired && !loggedIn) {
-        next('/login')
-    } else if (!authRequired && loggedIn) {
-        next('/')
-    } else {
-        next()
-    }
-})
-
-app.use(router)
-
 // 引入element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+
 // 引入element-plus全局国际化的配置
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-app.use(ElementPlus, {
-    locale: zhCn,
-})
 
-//全局样式文件
-import '@/assets/css/main.scss'
-import '@/assets/css/app.scss'
+const initApp = () => {
+    //全局样式文件
+    import '@/assets/css/main.scss'
+    import '@/assets/css/app.scss'
 
-// 引入App.vue，根组件
-import App from '@/App.vue'
+    // 引入App.vue，根组件
+    import App from '@/App.vue'
 
-app.mount('#app')
+    const pinia = createPinia()
+    pinia.use(piniaPluginPersistedstate)
+
+    const router = createRouter({
+        history: createWebHistory(import.meta.env.BASE_URL), routes: routes
+    })
+
+
+    router.beforeEach(async (to, from, next) => {
+        const authStore = useAuth()
+        const publicPages = ['/login', '/register']
+        const authRequired = !publicPages.includes(to.path)
+        const loggedIn = await authStore.current();
+
+        if (authRequired && !loggedIn) {
+            next('/login')
+        } else if (!authRequired && loggedIn) {
+            next('/')
+        } else {
+            next()
+        }
+    })
+
+    const app = createApp(App)
+
+    app.use(pinia)
+
+    app.use(router)
+
+    app.use(ElementPlus, {
+        locale: zhCn,
+    })
+
+    app.mount('#app')
+
+    return app
+}
+
+export default initApp
