cc47dce066939869fba42ad12176c78aa238ea5b
fair-gz <3194909424@qq.com>
2023-12-08 22:03:16 +0800

    minor fix

--- components/Resources/ResourceList.jsx
+++ components/Resources/ResourceList.jsx
@@ -76,121 +76,120 @@ export default defineComponent({
         expose({getResourceList, tableRef})
 
         return () => (
-            <>
-                <el-main>
-                    {
-                        props.listConfig.tabOptions &&
-                        <el-tabs className="resource-list-tabs" v-model={activeTab.value} onTabChange={onTabChange}>
-                            {
-                                props.listConfig.tabOptions.map((option, i) => (
-                                    <el-tab-pane label={option.label} name={i}/>
-                                ))
-                            }
-                        </el-tabs>
-                    }
+            <el-main>
+                {
+                    props.listConfig.tabOptions &&
+                    <el-tabs className="resource-list-tabs" v-model={activeTab.value} onTabChange={onTabChange}>
+                        {
+                            props.listConfig.tabOptions.map((option, i) => (
+                                <el-tab-pane label={option.label} name={i}/>
+                            ))
+                        }
+                    </el-tabs>
+                }
 
-                    <div v-show={displayFilter.value}>
-                        <h3 class="mb-20">搜索</h3>
+                <div v-show={displayFilter.value}>
+                    <h3 class="mb-20">搜索</h3>
 
-                        <ResourceForm ref={formRef} resource={{}}
-                                      labelPosition="left"
-                                      onSubmit={onFilterQuery}
-                                      columns={[
-                                          ...props.listConfig.filters,
-                                          ...[{
-                                              prop: 'actions',
-                                              type: 'display',
-                                              render: () => (
-                                                  <el-col span={8}>
-                                                      <div className={"d-flex align-center"}
-                                                           style={"margin:0 0 18px 50px;"}>
-                                                          <el-button type="primary"
-                                                                     onClick={() => formRef.value.submit()}
-                                                                     icon={Search}>
-                                                              {t('resources.search')}
-                                                          </el-button>
-                                                          <el-button onClick={onResetQuery}
-                                                                     icon={Refresh}>
-                                                              {t('resources.reset')}
-                                                          </el-button>
-                                                      </div>
-                                                  </el-col>
-                                              )
-                                          }]
-                                      ]}/>
+                    <ResourceForm ref={formRef} resource={{}}
+                                  labelPosition="left"
+                                  onSubmit={onFilterQuery}
+                                  columns={[
+                                      ...props.listConfig.filters,
+                                      ...[{
+                                          prop: 'actions',
+                                          type: 'display',
+                                          render: () => (
+                                              <el-col span={8}>
+                                                  <div className={"d-flex align-center"}
+                                                       style={"margin:0 0 18px 50px;"}>
+                                                      <el-button type="primary"
+                                                                 onClick={() => formRef.value.submit()}
+                                                                 icon={Search}>
+                                                          {t('resources.search')}
+                                                      </el-button>
+                                                      <el-button onClick={onResetQuery}
+                                                                 icon={Refresh}>
+                                                          {t('resources.reset')}
+                                                      </el-button>
+                                                  </div>
+                                              </el-col>
+                                          )
+                                      }]
+                                  ]}/>
 
-                        <el-divider/>
+                    <el-divider/>
 
+                </div>
+
+                <div class="row-justify-space-between mb-10">
+                    <h3>{props.listConfig.title}</h3>
+
+                    <div>
+                        <el-button icon={Refresh} circle onClick={() => getResourceList()}/>
+
+                        {props.listConfig.filters && props.listConfig.filters.length > 0 &&
+                            <el-button icon={Search} circle type={displayFilter.value ? 'primary' : 'default'}
+                                       onClick={onSearch}/>
+                        }
+
+                        {(!props.listConfig.actions || props.listConfig.actions.includes('new')) &&
+                            <el-button icon={Plus} type="primary" onClick={() => emit('new')}>
+                                {t('resources.new')}
+                            </el-button>
+                        }
+
+                        {/*{(!props.listConfig.actions || props.listConfig.actions.includes('delete')) &&*/}
+                        {/*    <el-button icon={Delete} plain type="danger" v-show={selectedIds.value.length}*/}
+                        {/*               onClick={onDeleteSelected}>{t('resources.delete')}*/}
+                        {/*    </el-button>}*/}
                     </div>
+                </div>
 
-                    <div class="row-justify-space-between mb-10">
-                        <h3>{props.listConfig.title}</h3>
+                <ResourceTable
+                    ref={tableRef}
+                    remote={(table_queries) => {
+                        let all_queries = queries.value;
 
-                        <div>
-                            <el-button icon={Refresh} circle onClick={() => getResourceList()}/>
+                        if (table_queries)
+                            all_queries = {...table_queries, ...all_queries};
 
-                            {props.listConfig.filters && props.listConfig.filters.length > 0 &&
-                                <el-button icon={Search} circle type={displayFilter.value ? 'primary' : 'default'}
-                                           onClick={onSearch}/>
-                            }
+                        if (props.listConfig.queries)
+                            all_queries = {...props.listConfig.queries, ...all_queries};
 
-                            {(!props.listConfig.actions || props.listConfig.actions.includes('new')) &&
-                                <el-button icon={Plus} type="primary" onClick={() => emit('new')}>
-                                    {t('resources.new')}
-                                </el-button>
-                            }
+                        if (props.listConfig.tabProp)
+                            all_queries[props.listConfig.tabProp] = props.listConfig.tabOptions[activeTab.value].value;
 
-                            {/*{(!props.listConfig.actions || props.listConfig.actions.includes('delete')) &&*/}
-                            {/*    <el-button icon={Delete} plain type="danger" v-show={selectedIds.value.length}*/}
-                            {/*               onClick={onDeleteSelected}>{t('resources.delete')}*/}
-                            {/*    </el-button>}*/}
-                        </div>
-                    </div>
+                        return API[props.resourceConfig.resourceData].all(all_queries)
+                    }}
+                    preprocess={props.listConfig.preprocess}
+                    tableProps={props.listConfig.tableProps}
+                    customTable={props.listConfig.customTable}
+                    columns={[
+                        ...props.listConfig.columns,
+                        ...[{
+                            label: t('resources.actions'),
+                            fixed: 'right',
+                            width: 150,
+                            render: (r) =>
+                                (r.id && <div>
+                                        {props.listConfig.actions.includes('show') &&
+                                            <el-button plain type="primary" icon={ZoomIn} circle
+                                                       onClick={() => emit('show', r)}/>}
 
-                    <ResourceTable
-                        ref={tableRef}
-                        remote={(table_queries) => {
-                            let all_queries = queries.value;
+                                        {props.listConfig.actions.includes('edit') &&
+                                            <el-button plain type="warning" icon={Edit} circle
+                                                       onClick={() => emit('edit', r)}/>}
 
-                            if (table_queries)
-                                all_queries = {...table_queries, ...all_queries};
+                                        {props.listConfig.actions.includes('delete') &&
+                                            <el-button plain type="danger" icon={Delete} circle
+                                                       onClick={() => onDelete(r)}/>}
+                                    </div>
+                                )
 
-                            if (props.listConfig.queries)
-                                all_queries = {...props.listConfig.queries, ...all_queries};
-
-                            if (props.listConfig.tabProp)
-                                all_queries[props.listConfig.tabProp] = props.listConfig.tabOptions[activeTab.value].value;
-
-                            return API[props.resourceConfig.resourceData].all(all_queries)
-                        }}
-                        preprocess={props.listConfig.preprocess}
-                        tableProps={props.listConfig.tableProps}
-                        columns={[
-                            ...props.listConfig.columns,
-                            ...[{
-                                label: t('resources.actions'),
-                                fixed: 'right',
-                                width: 150,
-                                render: (r) =>
-                                    (r.id && <div>
-                                            {props.listConfig.actions.includes('show') &&
-                                                <el-button plain type="primary" icon={ZoomIn} circle
-                                                           onClick={() => emit('show', r)}/>}
-
-                                            {props.listConfig.actions.includes('edit') &&
-                                                <el-button plain type="warning" icon={Edit} circle
-                                                           onClick={() => emit('edit', r)}/>}
-
-                                            {props.listConfig.actions.includes('delete') &&
-                                                <el-button plain type="danger" icon={Delete} circle
-                                                           onClick={() => onDelete(r)}/>}
-                                        </div>
-                                    )
-
-                            }]
-                        ]}/>
-                </el-main>
-            </>
+                        }]
+                    ]}/>
+            </el-main>
         )
     }
 })
