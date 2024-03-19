dd6ef4ace2c400b2d91e88ee4ce44630099a77a5
Lsun0731 <15650557602@163.com>
2024-03-19 19:53:36 +0800

    minor fix

--- components/ResourcesPage/ResourceList.jsx
+++ components/ResourcesPage/ResourceList.jsx
@@ -1,12 +1,19 @@
-import {ref, defineComponent} from "vue";
+import {ref, defineComponent, nextTick, onMounted} from "vue";
 import API from "@/api/index.js";
 import {ElMessage, ElMessageBox} from "element-plus";
 import {Delete, Edit, ZoomIn, Search, Refresh, Plus} from "@element-plus/icons-vue";
 import {useI18n} from 'vue-i18n'
 import {useRouter} from "vue-router";
 import ResourceForm from "../Resources/ResourceForm";
-import ResourceTable from "../Resources/ResourceTable";
+import ResourceTable from "../ResourcesPage/ResourceTable";
 import './ResourceList.scss';
+
+
+const paginate = (data, pageSize, currentPage) => {
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    return data.slice(startIndex, endIndex);
+}
 
 export default defineComponent({
     name: 'ResourceList',
@@ -16,12 +23,14 @@ export default defineComponent({
         },
         listConfig: {
             type: Object,
-        }
+        },
+        tableData: {
+            type: Array
+        },
     },
     emits: ['new', 'show', 'edit'],
     setup(props, {expose, emit}) {
         const router = useRouter()
-
         const {t} = useI18n()
 
         const activeTab = ref(props.listConfig.activeTab ? props.listConfig.activeTab : 0);
@@ -34,10 +43,19 @@ export default defineComponent({
 
         const queries = ref({});
 
+        const loading = ref(true);
+
+
+        const pageSize = ref(props.listConfig.page_size ? props.listConfig.page_size : 10);
+        const currentPage = ref(props.listConfig.current_page ? props.listConfig.current_page : 1);
+
+        const resources = ref([]);
+        const resourcesTotal = ref(0);
+
         const onDelete = (resource) => {
             ElMessageBox.confirm(t('resources.delete_prompt')).then(() => {
                 API[props.resourceConfig.resourceData].delete(resource.id).then(response => {
-                    tableRef.value.getResourceList();
+                    getResourceList();
                     ElMessage({
                         message: t('resources.delete_message'),
                         type: 'success',
@@ -46,21 +64,48 @@ export default defineComponent({
             })
         }
 
-        const getResourceList = () => tableRef.value.getResourceList();
+        const initPagination = () => {
+            currentPage.value = props.listConfig.current_page ? props.listConfig.current_page : 1
+            pageSize.value = props.listConfig.page_size ? props.listConfig.page_size : 10
+        }
 
+        const getResourceList = () => {
+            loading.value = true;
+            if (props.tableData) {
+                resources.value = paginate(props.tableData, pageSize.value, currentPage.value);
+                resourcesTotal.value = props.tableData.length;
+                loading.value = false;
+            } else {
+
+                let all_queries = getQueries();
+
+                all_queries = {page_size: pageSize.value, page: currentPage.value, ...all_queries};
+
+                API[props.resourceConfig.resourceData].all(all_queries).then(async response => {
+                    if (props.listConfig.preprocess) {
+                        response = props.listConfig.preprocess(response);
+                    }
+
+                    resources.value = response.data;
+                    resourcesTotal.value = response.total;
+
+                    loading.value = false;
+                })
+            }
+        }
 
         const onFilterQuery = (values, resolve) => {
             queries.value = values;
-            tableRef.value.initPagination();
-            tableRef.value.getResourceList();
+            initPagination();
+            getResourceList();
             resolve()
         }
 
         const onResetQuery = () => {
             formRef.value.reset();
+            initPagination();
             formRef.value.submit();
-            tableRef.value.initPagination();
-            tableRef.value.getResourceList();
+            // getResourceList();
         }
 
         const onSearch = () => {
@@ -72,7 +117,6 @@ export default defineComponent({
         }
 
         const onDeleteSelected = () => {
-
         }
 
         const onTabChange = () => {
@@ -93,138 +137,147 @@ export default defineComponent({
             return all_queries
         }
 
+        onMounted(() => {
+            getResourceList();
+        });
+
         expose({getResourceList, getQueries, tableRef})
 
         return () => (
-            <el-main>
-                {
-                    props.listConfig.tabOptions &&
-                    <el-tabs className="resource-list-tabs" v-model={activeTab.value} onTabChange={onTabChange}>
-                        {
-                            props.listConfig.tabOptions.map((option, i) => (
-                                <el-tab-pane label={option.label} name={i}/>
-                            ))
-                        }
-                    </el-tabs>
-                }
-
-                <div v-show={displayFilter.value}>
-                    <h3 class="mb-20">搜索</h3>
-
-                    <ResourceForm ref={formRef} resource={{}}
-                                  labelPosition="left"
-                                  onSubmit={onFilterQuery}
-                                  columns={[
-                                      ...props.listConfig.filters,
-                                      ...[{
-                                          prop: 'actions',
-                                          type: 'display',
-                                          render: () => (
-                                              <el-col span={8}>
-                                                  <div className={"d-flex align-center"}
-                                                       style={"margin:0 0 18px 50px;"}>
-                                                      <el-button type="primary"
-                                                                 onClick={() => formRef.value.submit()}
-                                                                 icon={Search}>
-                                                          {t('resources.search')}
-                                                      </el-button>
-                                                      <el-button onClick={onResetQuery}
-                                                                 icon={Refresh}>
-                                                          {t('resources.reset')}
-                                                      </el-button>
-                                                  </div>
-                                              </el-col>
-                                          )
-                                      }]
-                                  ]}/>
-
-                    <el-divider/>
-
-                </div>
-
-                <div class="row-justify-space-between mb-10">
-                    <h3>{props.listConfig.title}</h3>
-
-                    <div>
-                        <el-button icon={Refresh} circle onClick={() => getResourceList()}/>
-
-                        {props.listConfig.filters && props.listConfig.filters.length > 0 &&
-                            <el-button icon={Search} circle type={displayFilter.value ? 'primary' : 'default'}
-                                       onClick={onSearch}/>
-                        }
-
-                        {props.listConfig.tools && props.listConfig.tools()}
-
-                        {(!props.listConfig.actions || props.listConfig.actions.includes('new')) &&
-                            <el-button icon={Plus} type="primary" onClick={() => {
-                                emit('new')
-                                router.push({
-                                    path: `/${props.resourceConfig.resourcePath}/new`
-                                })
+            <div class={"d-column flex-1"} style="height:0" v-loading={loading.value}>
+                <el-main>
+                    {props.listConfig.tabOptions &&
+                        <el-tabs className="resource-list-tabs" v-model={activeTab.value} onTabChange={onTabChange}>
+                            {
+                                props.listConfig.tabOptions.map((option, i) => (
+                                    <el-tab-pane label={option.label} name={i}/>
+                                ))
                             }
-                            }>
-                                {t('resources.new')}
-                            </el-button>
-                        }
+                        </el-tabs>}
 
-                        {/*{(!props.listConfig.actions || props.listConfig.actions.includes('delete')) &&*/}
-                        {/*    <el-button icon={Delete} plain type="danger" v-show={selectedIds.value.length}*/}
-                        {/*               onClick={onDeleteSelected}>{t('resources.delete')}*/}
-                        {/*    </el-button>}*/}
+                    <div v-show={displayFilter.value}>
+                        <h3 class="mb-20">搜索</h3>
+
+                        <ResourceForm ref={formRef} resource={{}}
+                                      labelPosition="left"
+                                      onSubmit={onFilterQuery}
+                                      columns={[
+                                          ...props.listConfig.filters,
+                                          ...[{
+                                              prop: 'actions',
+                                              type: 'display',
+                                              render: () => (
+                                                  <el-col span={8}>
+                                                      <div className={"d-flex align-center"}
+                                                           style={"margin:0 0 18px 50px;"}>
+                                                          <el-button type="primary"
+                                                                     onClick={() => formRef.value.submit()}
+                                                                     icon={Search}>
+                                                              {t('resources.search')}
+                                                          </el-button>
+                                                          <el-button onClick={onResetQuery}
+                                                                     icon={Refresh}>
+                                                              {t('resources.reset')}
+                                                          </el-button>
+                                                      </div>
+                                                  </el-col>
+                                              )
+                                          }]
+                                      ]}/>
+
+                        <el-divider/>
+
                     </div>
-                </div>
 
-                <ResourceTable
-                    ref={tableRef}
-                    remote={(table_queries) => {
-                        let all_queries = getQueries();
+                    <div class="row-justify-space-between mb-10">
+                        <h3>{props.listConfig.title}</h3>
 
-                        if (table_queries)
-                            all_queries = {...table_queries, ...all_queries};
+                        <div>
+                            <el-button icon={Refresh} circle onClick={() => getResourceList()}/>
 
-                        return API[props.resourceConfig.resourceData].all(all_queries)
-                    }}
-                    preprocess={props.listConfig.preprocess}
-                    tableProps={props.listConfig.tableProps}
-                    customTable={props.listConfig.customTable}
-                    enableSelection={props.listConfig.enableSelection}
-                    columns={[
-                        ...props.listConfig.columns,
-                        ...[{
-                            label: t('resources.actions'),
-                            fixed: 'right',
-                            width: 150,
-                            render: (r) =>
-                                (r.id && <div>
-                                        {(!props.listConfig.actions || props.listConfig.actions.includes('show')) &&
-                                            <el-button plain type="primary" icon={ZoomIn} circle
-                                                       onClick={
-                                                           () => {
-                                                               emit('show', r)
+                            {props.listConfig.filters && props.listConfig.filters.length > 0 &&
+                                <el-button icon={Search} circle type={displayFilter.value ? 'primary' : 'default'}
+                                           onClick={onSearch}/>
+                            }
+
+                            {props.listConfig.tools && props.listConfig.tools()}
+
+                            {(!props.listConfig.actions || props.listConfig.actions.includes('new')) &&
+                                <el-button icon={Plus} type="primary" onClick={() => {
+                                    emit('new')
+                                    router.push({
+                                        path: `/${props.resourceConfig.resourcePath}/new`
+                                    })
+                                }
+                                }>
+                                    {t('resources.new')}
+                                </el-button>
+                            }
+
+                            {/*{(!props.listConfig.actions || props.listConfig.actions.includes('delete')) &&*/}
+                            {/*    <el-button icon={Delete} plain type="danger" v-show={selectedIds.value.length}*/}
+                            {/*               onClick={onDeleteSelected}>{t('resources.delete')}*/}
+                            {/*    </el-button>}*/}
+                        </div>
+                    </div>
+
+                    {!loading.value && <ResourceTable
+                        ref={tableRef}
+                        data={resources.value}
+                        tableProps={props.listConfig.tableProps}
+                        customTable={props.listConfig.customTable}
+                        enableSelection={props.listConfig.enableSelection}
+                        filterRowSelections={props.listConfig.filterRowSelections}
+                        enablePagination={false}
+                        columns={[
+                            ...props.listConfig.columns,
+                            ...[{
+                                label: t('resources.actions'),
+                                fixed: 'right',
+                                width: 150,
+                                render: (r) =>
+                                    (r.id && <div>
+                                            {(!props.listConfig.actions || props.listConfig.actions.includes('show')) &&
+                                                <el-button plain type="primary" icon={ZoomIn} circle
+                                                           onClick={
+                                                               () => {
+                                                                   emit('show', r)
+                                                                   router.push({
+                                                                       path: `/${props.resourceConfig.resourcePath}/show/${r.id}`
+                                                                   })
+                                                               }
+                                                           }/>}
+
+                                            {(!props.listConfig.actions || props.listConfig.actions.includes('edit')) &&
+                                                <el-button plain type="warning" icon={Edit} circle
+                                                           onClick={() => {
+                                                               emit('edit', r)
                                                                router.push({
-                                                                   path: `/${props.resourceConfig.resourcePath}/show/${r.id}`
+                                                                   path: `/${props.resourceConfig.resourcePath}/edit/${r.id}`
                                                                })
-                                                           }
-                                                       }/>}
+                                                           }}/>}
 
-                                        {(!props.listConfig.actions || props.listConfig.actions.includes('edit')) &&
-                                            <el-button plain type="warning" icon={Edit} circle
-                                                       onClick={() => {
-                                                           emit('edit', r)
-                                                           router.push({
-                                                               path: `/${props.resourceConfig.resourcePath}/edit/${r.id}`
-                                                           })
-                                                       }}/>}
+                                            {(!props.listConfig.actions || props.listConfig.actions.includes('delete')) &&
+                                                <el-button plain type="danger" icon={Delete} circle
+                                                           onClick={() => onDelete(r)}/>}
+                                        </div>
+                                    )
 
-                                        {(!props.listConfig.actions || props.listConfig.actions.includes('delete')) &&
-                                            <el-button plain type="danger" icon={Delete} circle
-                                                       onClick={() => onDelete(r)}/>}
-                                    </div>
-                                )
-
-                        }]
-                    ]}/>
-            </el-main>
-        )
+                            }]
+                        ]}/>}
+                </el-main>
+                <el-footer class={"footer"}>
+                    <div class={"h-100 row-center"}>
+                        <el-pagination v-model:pageSize={pageSize.value}
+                                       v-model:currentPage={currentPage.value}
+                                       pageSizes={[10, 50, 100]}
+                                       total={resourcesTotal.value}
+                                       layout="total,sizes, prev, pager, next, jumper"
+                                       onSizeChange={() => getResourceList()}
+                                       onCurrentChange={() => getResourceList()}
+                        />
+                    </div>
+                </el-footer>
+            </div>)
     }
 })
---
+++ components/ResourcesPage/ResourceTable.jsx
@@ -0,0 +1,191 @@
+import {ref, watch, defineComponent, onMounted, nextTick} from "vue";
+import {formatDateTime} from "../../config/tools";
+
+import _ from 'lodash-es';
+
+const renderItem = (column, scope) => {
+    let p = column.prop ? column.prop.split('.') : [];
+    let r = scope.row
+
+    if (column.render)
+        return column.render(r, column)
+    else if (column.type === 'image') {
+        return (
+            _.get(r, p) && <img height={70} src={_.get(r, p).src}/>
+        )
+    } else if (column.type === 'datetime') {
+        return <el-text>{formatDateTime(_.get(r, p))}</el-text>
+    } else if (column.type === 'price') {
+        return <el-text>${_.get(r, p).toFixed(2)}</el-text>
+    } else {
+        return (<el-text>{_.get(r, p)}</el-text>)
+    }
+}
+
+const paginate = (data, pageSize, currentPage) => {
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    return data.slice(startIndex, endIndex);
+}
+
+export default defineComponent({
+    name: 'ResourceTable',
+    props: {
+        columns: {
+            type: Array,
+            required: true
+        },
+        data: {
+            type: Array
+        },
+        remote: {
+            type: Function,
+        },
+        preprocess: {
+            type: Function
+        },
+        enableSelection: {
+            type: Boolean,
+            default: true
+        },
+        enablePagination: {
+            type: Boolean,
+            default: false
+        },
+        filterRowSelections: {
+            type: Function
+        },
+        page_size: {
+            type: Number,
+            default: 10
+        },
+        current_page: {
+            type: Number,
+            default: 1
+        },
+        tableProps: {
+            type: Object
+        },
+        customTable: {
+            type: Function
+        },
+    },
+    emits: ['selectionChange'],
+    setup(props, {expose, emit}) {
+
+        const tableRef = ref(null);
+        const loading = ref(false);
+
+        const resources = ref([]);
+        const resourcesTotal = ref(0);
+
+        const pageSize = ref(props.page_size);
+        const currentPage = ref(props.current_page);
+
+        const getResourceList = () => {
+            if (props.enablePagination) {
+                if (props.data) {
+                    resources.value = paginate(props.data, pageSize.value, currentPage.value);
+                    resourcesTotal.value = props.data.length;
+                } else if (props.remote) {
+                    loading.value = true;
+                    props.remote({page_size: pageSize.value, page: currentPage.value}).then(async response => {
+                        if (props.preprocess) {
+                            response = props.preprocess(response);
+                        }
+
+                        resources.value = response.data;
+                        resourcesTotal.value = response.total;
+
+                        nextTick().then(() => {
+                            if (tableRef.value && props.filterRowSelections) {
+                                props.filterRowSelections(resources.value).forEach(resource => {
+                                    tableRef.value.toggleRowSelection(resource, true);
+                                })
+                            }
+                        })
+
+                        loading.value = false;
+                    });
+                }
+            } else {
+                resources.value = props.data;
+
+                nextTick().then(() => {
+                    if (tableRef.value && props.filterRowSelections) {
+                        props.filterRowSelections(resources.value).forEach(resource => {
+                            tableRef.value.toggleRowSelection(resource, true);
+                        })
+                    }
+                })
+            }
+        }
+
+        const onSelectionChange = (rows) => {
+            emit('selectionChange', rows);
+        }
+
+        const initPagination = () => {
+            currentPage.value = props.current_page
+            pageSize.value = props.page_size
+        }
+
+        onMounted(() => {
+            getResourceList();
+        })
+
+        expose({getResourceList, initPagination})
+
+        watch(() => props.data, () => {
+            getResourceList()
+        })
+
+        return () => (
+            <>
+                {props.customTable ? props.customTable(resources.value) :
+                    <el-table data={resources.value}
+                              ref={tableRef}
+                              onSelectionChange={onSelectionChange}
+                              headerCellStyle={{textAlign: 'center'}}
+                              cellStyle={{textAlign: 'center'}}
+                              rowKey="id"
+                              {...props.tableProps}>
+                        {{
+                            default: () => <>
+                                {props.enableSelection && <el-table-column type="selection" width="55"/>}
+                                {props.columns.map(column => (
+                                    column.type === 'expand' ?
+                                        <el-table-column type="expand">
+                                            {{
+                                                default: (scope) => renderItem(column, scope)
+                                            }}
+                                        </el-table-column> :
+                                        <el-table-column width={column.width}>
+                                            {{
+                                                header: () => <el-text>{column.label}</el-text>,
+                                                default: (scope) => renderItem(column, scope)
+                                            }}
+                                        </el-table-column>
+                                ))}
+                            </>,
+                            empty: () => <el-empty/>
+                        }}
+                    </el-table>}
+
+                {props.enablePagination &&
+                    <div class="column-align-center">
+                        <el-divider></el-divider>
+                        <el-pagination v-model:pageSize={pageSize.value}
+                                       v-model:currentPage={currentPage.value}
+                                       pageSizes={[10, 50, 100]}
+                                       total={resourcesTotal.value}
+                                       layout="total,sizes, prev, pager, next, jumper"
+                                       onSizeChange={() => getResourceList()}
+                                       onCurrentChange={() => getResourceList()}
+                        />
+                    </div>
+                }
+            </>
+        )
+    }
+})
